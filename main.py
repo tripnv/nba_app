a861ee7eac305714c3253c3f3095066176f67ea4
N <nandibirta@gmail.com>
2021-10-24 04:47:08 +0300

    page looks reasonable; logic has been corrected

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
-from typing import OrderedDict
 from flask import Flask, render_template
 from nba_api.stats.endpoints import LeagueStandings
-import pandas as pd
+import json
+
+from numpy import log  
 app = Flask(__name__)
 
+with open('player_info.json', "r") as f:
+    players = json.load(f)
 
-players = {
-    "Albert":{
-        "east":["Nets","Bucks","Hawks","76ers","Heat","Bulls","Knicks","Celtics"],
-        "west":["Suns" ,"Lakers","Jazz","Mavericks","Warriors","Nuggets","Clippers","Trail Blazers"],
-        "points":None,
-        "points_total":None,
-    },
-    "Nandor":{
-        "east":["Nets","Bucks","Heat","Bulls","Knicks","76ers","Hawks","Celtics",],
-        "west":["Lakers","Jazz","Suns","Mavericks","Nuggets","Clippers","Warriors","Trail Blazers",],
-        "points":None,
-        "points_total":None,
-    },
-    "Gellert":{
-        "east":["Bucks","Nets","Hawks","Heat","Celtics","Bulls","76ers","Hornets",],
-        "west":["Jazz","Suns","Lakers","Nuggets","Mavericks","Warriors","Trail Blazers","Grizzlies"],
-        "points":None,
-        "points_total":None,
-    },
-    "Levente":{
-        "east":["Bucks","Nets","Hawks","Heat","76ers","Celtics","Knicks","Bulls"],
-        "west":["Lakers","Jazz","Suns","Mavericks,"	"Nuggets","Warriors","Clippers","Trail Blazers"],
-        "points":None,
-        "points_total":None,
-    }
-
-}
+with open('logos.json', "r") as f:
+    logos = json.load(f)
 
 def get_standings() -> tuple:
     req = LeagueStandings()
@@ -59,12 +37,11 @@ def assign_points(standings:tuple, players:dict) -> None:
     """
     east_top = [i[1] for i in standings[0]][:8]
     west_top = [i[1] for i in standings[1]][:8]
-    zeros = [0] * 8
+    
     for player in players.keys():
         players[player]["points"] = {"east_points":[0] * 8,"west_points":[0] * 8}
 
-    print(f"\n EAST")
-    print(f" ------------------------------------------------ ")          
+     
     for team in east_top:
         team_rank = east_top.index(team)
         for player in players:
@@ -73,21 +50,14 @@ def assign_points(standings:tuple, players:dict) -> None:
                 user_rank = players[player]["east"].index(team)
                 score_e = 10
                 
-                
                 if (user_rank < 4 and team_rank  < 4) or (user_rank >= 4 and team_rank >= 4):
                     score_e += 1
                     if user_rank == team_rank:
                         score_e += 2 
-                if abs(user_rank - team_rank) == 1:
-                    score_e += 1   
-                updated_points_e = copy.deepcopy(players[player]["points"]["east_points"])
-                updated_points_e[user_rank] = score_e
-                players[player]["points"]["east_points"] = updated_points_e
-            print(player, players[player]["points"]["east_points"], f"Team - {team} = {score_e} points")
-            #print(f"User {player} \n \tTeam: {team} \n \tUser_rank: {user_rank+1} \n \tTeam_rank: {team_rank+1} \n \tPoints: {score_1}\n")
+                
+                players[player]["points"]["east_points"][user_rank] = score_e
 
-    print(f"\n WEST")
-    print(f" ------------------------------------------------ ")          
+           
     for team in west_top:
         team_rank = west_top.index(team)
         for player in players.keys():
@@ -96,19 +66,13 @@ def assign_points(standings:tuple, players:dict) -> None:
                 user_rank = players[player]["west"].index(team)
                 score_w = 10
                 
-                
                 if (user_rank < 4 and team_rank  < 4) or (user_rank >= 4 and team_rank >= 4):
                     score_w += 1
                     if user_rank == team_rank:
                         score_w += 2 
-                if abs(user_rank - team_rank) == 1:
-                    score_w += 1
                 
-                updated_points_w = copy.deepcopy(players[player]["points"]["west_points"])
-                updated_points_w[user_rank] = score_w 
-                players[player]["points"]["west_points"] = updated_points_w
-            #print(f"User {player} \n \tTeam: {team} \n \tUser_rank: {user_rank+1} \n \tTeam_rank: {team_rank+1} \n \tPoints: {score_1}")
-            print(player, players[player]["points"]["west_points"], f"Team - {team} = {score_w} points")
+                players[player]["points"]["west_points"][user_rank] = score_w
+            
 
     for player in players.keys():
         players[player]["points_total"] = sum(players[player]["points"]["east_points"]) + sum(players[player]["points"]["west_points"])
@@ -118,7 +82,7 @@ def assign_points(standings:tuple, players:dict) -> None:
 def home():
     standings = get_standings()
     assign_points(standings, players)
-    return render_template("home.html", players = players)
+    return render_template("home.html", players = players, logos = logos)
 
 if __name__=="__main__":
     app.run(debug=True)
--- scrapbook.py
+++ scrapbook.py
@@ -2,7 +2,7 @@
 from nba_api.stats.endpoints import LeagueStandings
 import pandas as pd
 from collections import OrderedDict
-import copy
+import json
 #%%
 current_standings = LeagueStandings()
 current_standings
@@ -66,7 +66,7 @@ players = {
     },
     "Levente":{
         "east":["Bucks","Nets","Hawks","Heat","76ers","Celtics","Knicks","Bulls"],
-        "west":["Lakers","Jazz","Suns","Mavericks,"	"Nuggets","Warriors","Clippers","Trail Blazers"],
+        "west":["Lakers","Jazz","Suns","Mavericks","Nuggets","Warriors","Clippers","Trail Blazers"],
         "points":None,
         "points_total":None,
     }
@@ -93,7 +93,8 @@ east_top = [i[1] for i in standings[0]][:8]
 west_top = [i[1] for i in standings[1]][:8]
 
 # %%
-print(f"\n EAST ___________________")
+print(f"\n EAST")
+print(f" ------------------------------------------------ ")          
 for team in east_top:
     team_rank = east_top.index(team)
     for player in players:
@@ -107,15 +108,15 @@ for team in east_top:
                 score_e += 1
                 if user_rank == team_rank:
                     score_e += 2 
-                elif user_rank - team_rank == 1:
-                    score_e += 1
-            updated_points_e = copy.deepcopy(players[player]["points"]["east_points"])
-            updated_points_e[user_rank] = score_e
-            players[player]["points"]["east_points"] = updated_points_e
+            if abs(user_rank - team_rank) == 1:
+                score_e += 1   
+            players[player]["points"]["east_points"][user_rank] = score_e
+        
         print(player, players[player]["points"]["east_points"], f"Team - {team} = {score_e} points")
         #print(f"User {player} \n \tTeam: {team} \n \tUser_rank: {user_rank+1} \n \tTeam_rank: {team_rank+1} \n \tPoints: {score_1}\n")
 
-print(f"\n WEST ___________________")        
+print(f"\n WEST")
+print(f" ------------------------------------------------ ")          
 for team in west_top:
     team_rank = west_top.index(team)
     for player in players.keys():
@@ -129,22 +130,94 @@ for team in west_top:
                 score_w += 1
                 if user_rank == team_rank:
                     score_w += 2 
-                elif user_rank - team_rank == 1:
-                    score_w += 1
+            if abs(user_rank - team_rank) == 1:
+                score_w += 1
             
-            updated_points_w = copy.deepcopy(players[player]["points"]["west_points"])
-            updated_points_w[user_rank] = score_w 
-            players[player]["points"]["west_points"] = updated_points_w
+            players[player]["points"]["west_points"][user_rank] = score_w
         #print(f"User {player} \n \tTeam: {team} \n \tUser_rank: {user_rank+1} \n \tTeam_rank: {team_rank+1} \n \tPoints: {score_1}")
         print(player, players[player]["points"]["west_points"], f"Team - {team} = {score_w} points")
 
 #%%
 print(east_top)
+print(west_top)
 # %%
-#for player in players.keys():
-#    players[player]["points_total"] = sum(players[player]["points"]["east_points"]) + sum(players[player]["points"]["west_points"])
+for player in players.keys():
+    players[player]["points_total"] = sum(players[player]["points"]["east_points"]) + sum(players[player]["points"]["west_points"])
 # %%
 players
 # %%
 player_df = pd.DataFrame(columns = ["Albert_E","Nandor","Gellert", "Levente"])
+# %%
+import json
+# %%
+players = {
+    "Albert":{
+        "east":["Nets","Bucks","Hawks","76ers","Heat","Bulls","Knicks","Celtics"],
+        "west":["Suns" ,"Lakers","Jazz","Mavericks","Warriors","Nuggets","Clippers","Trail Blazers"],
+        "points":None,
+        "points_total":None,
+    },
+    "Nandor":{
+        "east":["Nets","Bucks","Heat","Bulls","Knicks","76ers","Hawks","Celtics",],
+        "west":["Lakers","Jazz","Suns","Mavericks","Nuggets","Clippers","Warriors","Trail Blazers",],
+        "points":None,
+        "points_total":None,
+    },
+    "Gellert":{
+        "east":["Bucks","Nets","Hawks","Heat","Celtics","Bulls","76ers","Hornets",],
+        "west":["Jazz","Suns","Lakers","Nuggets","Mavericks","Warriors","Trail Blazers","Grizzlies"],
+        "points":None,
+        "points_total":None,
+    },
+    "Levente":{
+        "east":["Bucks","Nets","Hawks","Heat","76ers","Celtics","Knicks","Bulls"],
+        "west":["Lakers","Jazz","Suns","Mavericks",	"Nuggets","Warriors","Clippers","Trail Blazers"],
+        "points":None,
+        "points_total":None,
+    },
+    "Pali":{
+        "east":["Nets", "76ers", "Hawks", "Bucks", "Bulls", "Pacers", "Celtics", "Cavaliers"],
+        "west":["Warriors", "Nuggets", "Clippers", "Mavericks", "Lakers", "Suns", "Trail Blazers","Grizzlies"],
+        "points":None,
+        "points_total":None,
+    },
+    "Ricsi":{
+        "east":["Bucks", "Nets","76ers", "Hawks", "Bulls", "Raptors", "Heat", "Pacers"],
+        "west":["Suns", "Lakers", "Clippers", "Jazz", "Nuggets", "Mavericks", "Trail Blazers", "Spurs"],
+        "points":None,
+        "points_total":None,
+    }, 
+    "Tamas":{
+        "east":["Bucks", "Nets", "76ers", "Celtics", "Hawks", "Bulls", "Wizards", "Knicks"],
+        "west":["Jazz","Lakers", "Suns", "Mavericks", "Warriors", "Clippers", "Nuggets", "Spurs"],
+        "points":None,
+        "points_total":None,
+    }
+}
+# %%
+with open("player_info.json", "w") as fp:
+    json.dump(players, fp)
+# %%
+
+with open('player_info.json', "r") as f:
+    data = json.load(f)
+# %%
+data
+# %%
+all_teams = []
+for person in data.keys():
+    all_teams.extend(data[person]["east"])
+    all_teams.extend(data[person]["west"])
+# %%
+len(all_teams)
+# %%
+all_teams.unique()
+# %%
+teams = pd.DataFrame(all_teams, columns=["team_name"])
+# %%
+len(teams["team_name"].unique())
+# %%
+with open("logos.json", "w") as fp:
+    json.dump(dict.fromkeys(teams["team_name"].unique()), fp)
+
 # %%
